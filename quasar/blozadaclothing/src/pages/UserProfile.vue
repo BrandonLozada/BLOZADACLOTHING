aa36f7b8db7df5fd41cfaa8f5d067d34fe55e267
BrandonLozada <brandon_lozada@outlook.com>
2022-12-16 15:04:36 -0600

    Agrgar ruta de UserProfile y modificar IndexPage

---
+++ quasar/blozadaclothing/src/pages/UserProfile.vue
@@ -0,0 +1,193 @@
+<template>
+  <q-page class="row items-center justify-evenly" style="max-width: 1100px; margin: 0 auto;">
+    <div class="col-12 col-md-10 col-lg-10" v-bind:class="$q.screen.lt.sm ? 'q-py-md' : 'q-pa-xl'">
+      <div class="text-center">
+        <p style="font-size: 30px; margin: 0 0 2px 0;">Completar perfil</p>
+        <p class="text-grey-8" style="font-size: 0.875rem; font-weight: 400;">Completa y asegúrate que tus datos sean correctos</p>
+      </div>
+          <div class="row justify-center align-center">
+            <div class="col-xs-12">
+              <div class="q-py-sm">
+
+<!--                      @submit.prevent="submitForm"-->
+                    <q-form
+                      ref="formularioPerfil"
+                      greedy
+                      class="q-gutter-lg">
+
+                        <q-card-section class="q-gutter-md">
+
+                          <q-input
+                            v-model="formData['first_name']"
+                            hint=""
+                            label="Nombre(s)"
+                            type="text" />
+
+                          <q-input
+                            v-model="formData['last_name']"
+                            hint=""
+                            label="Apellidos"
+                            type="text" />
+
+                          <q-input
+                            v-model="formData['birthdate']"
+                            hint=""
+                            label="Fecha de Nacimiento"
+                            type="date" />
+
+                          <q-input
+                            v-model="formData['cellphone']"
+                            hint=""
+                            label="Número de celular"
+                            type="text" />
+
+                          <div class="flex justify-end">
+                            <q-btn no-caps type="submit" color="primary" label="Guardar" class="bg-accent"/>
+                          </div>
+                        </q-card-section>
+
+                    </q-form>
+              </div>
+            </div>
+          </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+// import {useQuasar, event, date} from 'quasar'
+// import {useRouter, useRoute} from 'vue-router'
+//
+// // import {api, axios} from 'boot/axios'
+//
+// // import {useAuthStore} from 'stores/auth'
+// // import {useContextStore} from 'stores/SiteContextStore'
+//
+// // const authStore = useAuthStore()
+//
+// const {stopAndPrevent} = event
+//
+// const $q = useQuasar()
+//
+// // const siteContext = useContextStore()
+// // const router = useRouter()
+// // const route = useRoute()
+// // siteContext.current_page = route.path
+//
+// const submitResult = ref([])
+// const responseStatus = ref(false)
+// const responseMessage = ref('Error: ')
+//
+// const emailPattern = /^(?=[a-zA-Z0-9@.%+-]{6,254}$)[a-zA-Z0-9.%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/
+// const datePattern = /^(0[1-9]|[1-2]\d|3[01])(\/)(0[1-9]|1[012])\2(\d{4})$/
+// let fecha_convertida = ref('');
+//
+const formData = ref({})
+//
+// const isValidEmail = (val: string) => {
+//   if (!!val) {
+//     return emailPattern.test(val) || 'La dirección email no tiene formato correcto';
+//   } else {
+//     return 'Correo electrónico obligatorio'
+//   }
+// }
+//
+// let isValidDate = (val: string) => {
+//   if (!!val && val !== '') {
+//     /* Comprobar formato dd/mm/yyyy, que el no sea mayor de 12 y los días mayores de 31 */
+//     if (!val.match(datePattern)) {
+//       return 'La fecha no tiene formato'
+//     } else {
+//       /* Comprobar los días del mes */
+//       const day = parseInt(val.split('/')[0]);
+//       const month = parseInt(val.split('/')[1]);
+//       const year = parseInt(val.split('/')[2]);
+//       const monthDays = new Date(year, month, 0).getDate();
+//       if (day > monthDays) {
+//         return 'La fecha es inválida'
+//       }
+//       /* Comprobar que la fecha no sea superior a la actual*/
+//       const timeStamp = Date.now();
+//       const currentDate = date.formatDate(timeStamp, 'DD/MM/YYYY');
+//       const diferencia = date.getDateDiff(val, currentDate, 'days');
+//
+//       if (diferencia > 0 ) {
+//         return 'La fecha no puede ser superior a la actual'
+//       }
+//       //return 'Fecha válida'
+//       //fecha_convertida.value = date.formatDate(date.buildDate({ year, month, day}), 'YYYY/MM/DD');
+//     }
+//   } else {
+//     return 'El campo es requerido'
+//   }
+// }
+//
+// let convertDate = (val: string) => {
+//   const day = parseInt(val.split('/')[0]);
+//   const month = parseInt(val.split('/')[1]);
+//   const year = parseInt(val.split('/')[2]);
+//
+//   return date.formatDate(date.buildDate({ year, month, day}), 'YYYY/MM/DD');
+// }
+//
+// const myLocale = {
+//   /* starting with Sunday */
+//   days: 'Domingo_Lunes_Martes_Miércoles_Jueves_Viernes_Sábado'.split('_'),
+//   daysShort: 'Dom_Lun_Mar_Mié_Jue_Vie_Sáb'.split('_'),
+//   months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
+//   monthsShort: 'Ene_Feb_Mar_Abr_May_Jun_Jul_Ago_Sep_Oct_Nov_Dic'.split('_'),
+//   firstDayOfWeek: 1, // 0-6, 0 - Sunday, 1 Monday, ...
+//   format24h: true,
+//   pluralDay: 'dias'
+// }
+//
+// const showNotification = (
+//   message: string,
+//   color: string,
+//   actions: {
+//     label: string;
+//     color: string;
+//     handler: () => void }[] | undefined) => {
+//   $q.notify({
+//         message: message,
+//         color: color,
+//         actions: actions
+//   })
+// }
+//
+// const showLoadingBar = (message: string) => {
+//   $q.loading.show({
+//     message: message
+//   })
+// }
+
+// const submitForm = () => {
+//   formData.value.user = authStore.firebaseUserData.uid;
+//
+//   showLoadingBar('Estamos enviando la información. Espere un momento por favor...' )
+//
+//   // api.put(`/profiles/my/profile/${authStore.firebaseUserData.uid}/`,
+//   axios.put(`https://api.empleo.testing.monterrey.gob.mx/api/v1/profiles/my/profile/${authStore.firebaseUserData.uid}/`, formData.value, {
+//     headers: {
+//       'Authorization': 'JWT ' + authStore.firebaseUserData.accessToken,
+//     }
+//   }).then((response: { status: string | number; }) => {
+//     if (response.status === 201) {
+//       responseStatus.value = true
+//       responseMessage.value = 'Respuesta enviada exitosamente!'
+//     } else {
+//       responseMessage.value = responseMessage.value + response.status
+//     }
+//     $q.loading.hide()
+//     showNotification(response.status === 200 ? 'Perfil actualizado exitosamente' : 'Ocurrió un error: ' + response.status, response.status === 200 ? 'green' : 'red', [])
+//     setTimeout(() => {router.push(response.status === 201 || response.status === 200 ? '/inicio' : '/')},1000)
+//       }).catch((error: string) => {
+//         showNotification('Ocurrió un error' + error, 'red', [
+//           { label: 'Aceptar', color: 'white', handler: () => { /* ... */ } }
+//         ])
+//     $q.loading.hide()
+//       })
+// }
+
+</script>
