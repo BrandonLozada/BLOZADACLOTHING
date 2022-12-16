c1741e41a23c61a6201a3fc65479aede80849e7a
BrandonLozada <brandon_lozada@outlook.com>
2022-12-16 15:20:08 -0600

    Inputs a la biblioteca de comnponentes

---
+++ quasar/blozadaclothing/src/components/Inputs/EntryBlock.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="q-py-lg">
+    <!-- Entry Block -->
+    <q-input
+      dense
+      outlined
+      :label="label"
+      :v-model="modelValue"
+      :type="field_type"
+      :hint="help_text"
+      :name="clean_name"
+      :rules="required ? [val => !!val || `Campo requerido` + `: ${help_text}`] : ''"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface EntryBlockProps {
+  label: string;
+  modelValue?: never; // modelValue?: any;
+  field_type?: string;
+  help_text?: string;
+  clean_name?: string;
+  required?: string;
+}
+withDefaults(defineProps<EntryBlockProps>(), {
+  label: '',
+  field_type: 'text',
+  help_text: '',
+  // required: '',
+});
+</script>
