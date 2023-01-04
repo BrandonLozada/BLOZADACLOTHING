b08fdd6c6bafda5b8a1a3b773c46e0788382571e
BrandonLozada <brandon_lozada@outlook.com>
2023-01-04 05:17:24 -0600

    Implementación Footer - WIP 2

--- quasar/blozadaclothing/src/components/common/PrincipalFooter.vue
+++ quasar/blozadaclothing/src/components/common/PrincipalFooter.vue
@@ -1,85 +1,103 @@
 <template>
-  <q-footer reveal bordered class="bg-grey-11 text-white">
+  <q-footer class="bg-grey-11">
 
     <div class="justify-center bg-grey-10 text-grey-9 q-pa-md q-py-xl">
-      <div class="max-width-content q-mx-auto bg-grey-8">
-
+      <div class="max-width-content q-mx-auto">
         <div class="row">
 
-          <div class="bg-grey-7 col-6 col-md-3">
-            <div class="full-height q-pb-xl">
+          <div class="col-12 col-sm-4 col-sm-12 col-md-3 col-lg-3 q-pa-sm q-pb-xl">
+<!--            <div class="q-pa-sm text-left">-->
+              <a
+                :href="'/'"
+                title="B Lozada Clothing."
+              >
+                <q-img
+                  class=""
+                  style="max-width: 170px; max-height: 80px;"
+                  height="170"
+                  width="200"
+                  fit="contain"
+                  src="/img/logos/spotify_logo.png"
+                  alt="B Lozada Clothing."
+                />
+              </a>
+<!--            </div>-->
+          </div>
 
-              <q-card-section class="text-left text-green-6">
-                <div class="bg-white">
-                  <a
-                    :href="'/'"
-                    title="B Lozada Clothing."
-                  >
-                    <q-img
-                      class="bg-grey-5"
-                      style="max-width: 200px; max-height: 100px;"
-                      height="100"
-                      width="200"
-                      fit="contain"
-                      src="/img/logos/spotify_logo.png"
-                      alt="B Lozada Clothing."
-                    />
-                  </a>
-                </div>
-              </q-card-section>
+          <div class="col-12 col-sm-4 col-md-2 col-lg-2 q-pa-sm">
+            <div class="full-height q-pb-xl text-grey-1">
+
+              <div class="text-subtitle2">COMPAÑIA</div>
+              <ul class="q-pl-none" style="list-style-type: none;">
+                <template
+                  v-for="(item, index) in company"
+                  :key="index">
+                  <li class="q-px-none">
+                    <a :href="item.meta.slug" target="_blank">{{item.title}}</a>
+                  </li>
+                </template>
+              </ul>
 
             </div>
           </div>
 
-          <div class="bg-grey-6 col-12 col-md-3">
-            <div class="full-height q-pb-xl">
+          <div class="col-12 col-sm-4 col-md-2 col-lg-2 q-pa-sm">
+            <div class="full-height q-pb-xl text-grey-1">
 
-              <q-card-section class="text-left text-green-6">
-                <div class="text-subtitle2">COMPAÑIA</div>
-                <ul class="q-pl-none" style="list-style-type: none;">
-                  <template
-                    v-for="(item, index) in company"
-                    :key="index">
-                    <li class="q-px-none">
-                      <a :href="item.meta.slug" target="_blank">{{item.title}}</a>
-                    </li>
-                  </template>
-                </ul>
-              </q-card-section>
+              <div class="text-subtitle2">ENLACES ÚTILES</div>
+              <ul class="q-pl-none" style="list-style-type: none;">
+                <template
+                  v-for="(item, index) in usefulLinks"
+                  :key="index">
+                  <li class="q-px-none">
+                    <a :href="item.meta.slug" target="_blank">{{item.title}}</a>
+                  </li>
+                </template>
+              </ul>
 
             </div>
           </div>
 
-          <div class="bg-grey-5 col-12 col-md-3">
-            <div class="full-height q-pb-xl">
+          <div class="col-12 col-sm-4 col-md-2 col-lg-2 q-pa-sm">
+            <div class="full-height q-pb-xl text-grey-1">
 
-                <q-card-section class="text-green-6">
-                  <div class="text-subtitle2">ENLACES ÚTILES</div>
-                  <ul class="q-pl-none" style="list-style-type: none;">
-                    <template
-                      v-for="(item, index) in usefulLinks"
-                      :key="index">
-                      <li class="q-px-none">
-                        <a :href="item.meta.slug" target="_blank">{{item.title}}</a>
-                      </li>
-                    </template>
-                  </ul>
-                </q-card-section>
+              <div class="text-subtitle2">BLOG</div>
+              <ul class="q-pl-none" style="list-style-type: none;">
+                <template
+                  v-for="(item, index) in blog"
+                  :key="index">
+                  <li class="q-px-none">
+                    <a :href="item.meta.slug" target="_blank">{{item.title}}</a>
+                  </li>
+                </template>
+              </ul>
 
             </div>
           </div>
 
-          <div class="bg-grey-4 col-12 col-md-3">
-            <div class="full-height q-pb-xl">
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 q-pa-sm">
+              <div class="full-height q-pb-lg" v-bind:class="$q.screen.lt.sm ? '' : $q.screen.lt.md ? 'text-center' : 'text-right'">
 
-              <SocialNetworks
-                class="text-green-6"
-                v-for="item in socialNetworks"
-                :key="item.title"
-                v-bind="item"
-                size="lg"
-              />
+                <SocialNetworks
+                  class="text-grey-1"
+                  v-for="item in socialNetworks"
+                  :key="item.title"
+                  v-bind="item"
+                  size="22px"
+                />
 
+              </div>
+            </div>
+
+          <div class="col-12 col-md-12 bg-cyan-1 q-pa-sm">
+            <div class="text-subtitle2 text-weight-light text-right">
+              México
+            </div>
+          </div>
+
+          <div class="col-12 col-md-12 bg-cyan-3 q-pa-sm">
+            <div class="text-subtitle2 text-weight-light" v-bind:class="$q.screen.lt.sm ? '' : 'text-right'">
+              © 2023 - B Lozada Clothing.
             </div>
           </div>
 
@@ -93,10 +111,11 @@
 import {EssentialLinkProps} from 'components/EssentialLink.vue';
 import SocialNetworks, {SocialNetworksProps} from 'components/SocialNetwork.vue';
 
-// Redes sociales
-    // Facebook
-    // YouTube - opcional
-    // Instagram
+// Compañia
+    // Acerca de
+    // Contacto: Ubicación, Nosotros (Emprendurismo -> Misión, Visión, Valores)
+    // Contacto
+    // Patrocinio
 // Blog
     // Noticias
 // Contacto
@@ -106,15 +125,14 @@ import SocialNetworks, {SocialNetworksProps} from 'components/SocialNetwork.vue'
     // Celular: (+52) 818-025-1208 = href tel:+528180251208
 // Chat
     // Messenger
-// Enlaces útiles (Directo a la página final)
+// Enlaces útiles
     // Atención al cliente
     // Configuración
     // Ayuda
-// Compañia
-    // Acerca de
-    // Contacto: Nosotros (Emprendurismo -> Misión, Visión, Valores)
-    // Contacto
-    // Patrocinio
+// Redes sociales
+    // Facebook
+    // YouTube - opcional
+    // Instagram
 
 // Aviso de privacidad | Transparencia
 // Seguido de la empresa dueña o marca de la pagina, Copyright (R) 2023. Mty, NL, México
@@ -123,6 +141,15 @@ import SocialNetworks, {SocialNetworksProps} from 'components/SocialNetwork.vue'
 // Uso de Cookies y caché, (rastreo y seguimiento de datos)
 // Me gusta el aviso de abajo
 // https://www.videojet.mx/mx/homepage/resources/glossary/product-tracking-traceability/product-tracking.html
+
+const blog: EssentialLinkProps[] = [
+  {
+    title: 'Noticias',
+    meta: {
+      slug: '/blozada-blog',
+    }
+  }
+];
 
 const company: EssentialLinkProps[] = [
   {
