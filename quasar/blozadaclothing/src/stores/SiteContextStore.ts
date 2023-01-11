2e07214656a46585111010f00076d2efdfa321f0
BrandonLozada <brandon_lozada@outlook.com>
2023-01-11 04:04:00 -0600

    Cambiar variable del State de Pinia

--- quasar/blozadaclothing/src/stores/SiteContextStore.ts
+++ quasar/blozadaclothing/src/stores/SiteContextStore.ts
@@ -2,9 +2,8 @@ import { defineStore } from 'pinia'
 
 export const useContextStore = defineStore('SiteContextStore', {
   state: () => ({
-    selected_product: toString(),
-    current_page: toString(),
-    first_time: null,
+    selectedProduct: toString(),
+    currentPage: toString(),
   }),
 
   actions: {
